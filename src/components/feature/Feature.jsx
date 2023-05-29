9760e47158d93a1192da9bd8b6e0f644cc111500
Shawn Ruby <snyrby@gmail.com>
2023-05-28 20:21:39 -0700

    Finish brand
    working on whatgpt3 and feature components

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -3,7 +3,15 @@ import './feature.css';
 
 const Feature = () => {
   return (
-    <div>Feature</div>
+    <div className="gpt3__features-container_feature">
+      <div className="gpt3__features-container_feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        {text}
+      </div>
+    </div>
   )
 }
 
